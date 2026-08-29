b6021259d8618c5be1a40d9d6e888d7d4cc27477
Vasco Fernandes <vasco.miguel.df@gmail.com>
2026-08-29 17:51:46 +0100

    Add NLP experiments and model evaluation

--- src/chef_classifier/models.py
+++ src/chef_classifier/models.py
@@ -1,12 +1,22 @@
+from typing import Any
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
 
 
-def build_tfidf_svc_pipeline() -> Pipeline:
+def build_tfidf_svc_pipeline(
+    **tfidf_kwargs: Any,
+) -> Pipeline:
     return Pipeline(
         [
-            ("tfidf", TfidfVectorizer()),
-            ("classifier", LinearSVC()),
+            (
+                "tfidf",
+                TfidfVectorizer(**tfidf_kwargs),
+            ),
+            (
+                "classifier",
+                LinearSVC(),
+            ),
         ]
     )
